64a1176ffe1f070e0b7e2722e3c731dec06730a7
mbelesiu <matt.belesiu@gmail.com>
2020-11-03 14:38:19 -0800

    voice recongition happening

--- client/components/Dictaphone.jsx
+++ client/components/Dictaphone.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 const Dictaphone = () => {
   const { transcript, resetTranscript } = useSpeechRecognition()
-  const listen = [];
+  const [listen, setListen] = useState([]);
 
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return null
   }
   SpeechRecognition.startListening({ continuous: true })
-  listen.push(transcript.split(" ").pop())
-  console.log(Array.isArray(transcript))
+  useEffect(() => {
+    const newKeyWords = transcript.split(" ").slice(-2)
+    setListen(newKeyWords)
+    if (listen.length >= 2) {
+      if (listen[0].toLowerCase() === 'hey' && listen[1].toLowerCase() === 'anna') {
+        console.log('Hey Anna');
+        resetTranscript();
+        setListen(['','']);
+      }
+      if (listen[0].toLowerCase() === 'stop' && listen[1].toLowerCase() === 'listening') {
+        console.log('Stop listening');
+        resetTranscript();
+        setListen(['','']);
+      }
+    }
+
+  }, [listen])
+
 
   return (
     <div>
       {/* <button onClick={SpeechRecognition.startListening({ continuous: true })}>Start</button> */}
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
-      <p>{listen}</p>
+      <p>{listen[0]}{listen[1]}</p>
     </div>
   )
 }
