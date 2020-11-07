2db545b5649242af3300df2b6495ccbde090602a
mbelesiu <matt.belesiu@gmail.com>
2020-11-07 08:42:28 -0800

    example configs for git

---
+++ database/config.copy.js
@@ -0,0 +1,8 @@
+// ArangoDB login Config. I used Gmail for this build, so this file may change for your email serivce
+const credentials = {
+  username: 'FILL_ME_IN',
+  password: 'FILL_ME_IN',
+  database: '_system',
+};
+
+module.exports = credentials;
---
+++ server/config.copy.js
@@ -0,0 +1,8 @@
+// Email Config. I used Gmail for this build, so this file may change for your email serivce
+const credentials = {
+  email: 'FILL_ME_IN',
+  password: 'FILL_ME_IN',
+
+};
+
+module.exports = credentials;
