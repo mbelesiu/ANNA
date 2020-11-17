843c9c101908c79f51e9837f8edded628e8ee9d7
mbelesiu <matt.belesiu@gmail.com>
2020-11-17 15:07:05 -0800

    non-functional changePrompt Modal

--- client/components/ChangePrompts.jsx
+++ client/components/ChangePrompts.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min';
+import { Modal, Button, Textarea } from 'react-materialize';
 import styled from 'styled-components';
 
 function ChangePrompts({ prompts, show, changeShow, getPrompts, currentUser}) {
@@ -26,51 +29,51 @@ function ChangePrompts({ prompts, show, changeShow, getPrompts, currentUser}) {
   if (!show) {
     return null;
   }
-  console.log(promptList)
   return (
-    <Modal>
-      <ModalContent>
-        <h5>Change Prompts for future entries</h5>
+    <Modal
+      actions={[
+        <Button flat modal="close" node="button" waves="green">Nevermind...</Button>
+      ]}
+      bottomSheet={false}
+      fixedFooter={false}
+      header="Change Prompts for Future Entries"
+      id="Modal-0"
+      open={true}
+      options={{
+        dismissible: false,
+        endingTop: '10%',
+        inDuration: 250,
+        onCloseEnd: (() => (changeShow(false))),
+        onCloseStart: (() => (setPromptList([]))),
+        onOpenEnd: null,
+        onOpenStart: null,
+        opacity: 0.5,
+        outDuration: 250,
+        preventScrolling: true,
+        startingTop: '4%'
+      }}
+    >
+
         <form onSubmit={(e) => { handleSubmit(e) }}>
           {promptList.map((prompt, i) =>
-            <label> <h6>Current Prompt {i + 1}: {prompt}</h6>
-              <input
+            <label> <h6>Current Prompt {i + 1}</h6>
+              <Textarea
                 name={prompt}
+                type="text"
                 value={prompt}
                 onChange={(e) => handleChange(e.target.value, i)}
               />
             </label>
           )}
 
-          <button>Submit Change</button><button onClick={(e) => { changeShow(false) }}>Cancel</button>
+          <Button>Submit Change</Button>
         </form>
-      </ModalContent>
+
     </Modal>
 
   )
 }
 
-const Modal = styled.div`
-
-  position: fixed;
-  z-index: 1;
-  padding-top: 100px;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  overflow: auto;
-  background-color: rgb(0,0,0);
-  background-color: rgba(0,0,0,0.4);
-`;
-
-const ModalContent = styled.div`
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-`;
 
 
 export default ChangePrompts;
