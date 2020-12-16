5d7323ee7afebc85a0287f7c65710194467734f8
mbelesiu <matt.belesiu@gmail.com>
2020-12-16 12:15:12 -0800

    login revamped

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,33 +1,33 @@
-const db = require('../../database');
-//const db = require('../../postgres')
+//const db = require('../../database');
+const db = require('../../postgres')
 const aqlQuery = require('arangojs').aqlQuery;
 
-const loginMethods =
-{
-  login : (req, res) => {
-  const username = req.params.username;
-  db.query(aqlQuery`FOR u IN Users FILTER u.email == ${username} RETURN u`)
-    .then(({ _result }) => {
-      if (_result.length === 0) {
-        db.query(aqlQuery`INSERT { email: ${username}, prompts: {} } INTO Users`)
-          .then(() => db.query(aqlQuery`INSERT { email: ${username}, entry: [] } INTO Records`))
-          .then(() => {
-             res.sendStatus(200)
-          })
-          .catch((err) => {
-            console.log(err)
-            res.sendStatus(404)
-          })
-      } else {
-        res.send(_result)
-      }
+// const loginMethods =
+// {
+//   login : (req, res) => {
+//   const username = req.params.username;
+//   db.query(aqlQuery`FOR u IN Users FILTER u.email == ${username} RETURN u`)
+//     .then(({ _result }) => {
+//       if (_result.length === 0) {
+//         db.query(aqlQuery`INSERT { email: ${username}, prompts: {} } INTO Users`)
+//           .then(() => db.query(aqlQuery`INSERT { email: ${username}, entry: [] } INTO Records`))
+//           .then(() => {
+//              res.sendStatus(200)
+//           })
+//           .catch((err) => {
+//             console.log(err)
+//             res.sendStatus(404)
+//           })
+//       } else {
+//         res.send(_result)
+//       }
 
-    })
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(404)
-    })
-}}
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.sendStatus(404)
+//     })
+// }}
 
 const postgressLoginMethods =
 {
@@ -58,4 +58,4 @@ const postgressLoginMethods =
 
 
 
-module.exports = loginMethods;
+module.exports = postgressLoginMethods;
