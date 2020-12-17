c6c9c1f6373592a714a82754a356aa0486872806
mbelesiu <matt.belesiu@gmail.com>
2020-12-17 12:13:59 -0800

    close

--- server/routes/records.js
+++ server/routes/records.js
@@ -2,35 +2,6 @@
 // const aqlQuery = require('arangojs').aqlQuery;
 const db = require('../../postgres'); // postgres
 
-const recordMethods = {
-  getRecords: (req, res) => {
-    const username = req.params.username
-    db.query(aqlQuery`FOR u IN Records FILTER u.email == ${username} RETURN u`)
-      .then(({ _result }) => res.send(_result))
-      .catch((err) => {
-        console.log(err);
-        res.sendStatus(404)
-      })
-  },
-  createRecords: (req, res) => {
-    const username = req.params.username;
-    const newEntry = req.body;
-
-    db.query(aqlQuery`FOR u IN Records FILTER u.email == ${username} RETURN u`)
-      .then(({ _result }) => {
-        const currentEntries = _result[0].entry
-        console.log(currentEntries);
-        newEntry['id'] = currentEntries.length + 1;
-        currentEntries.push(newEntry);
-        db.query(aqlQuery`FOR u IN Records FILTER u.email == ${username} UPDATE u WITH {entry: ${currentEntries}} IN Records RETURN u`)
-          .then(() => res.sendStatus(200))
-      })
-      .catch((err) => {
-        console.log(err)
-        res.sendStatus(404);
-      })
-  }
-};
 
 const postgresRecordMethods = {
   getRecords: (req, res) => {
@@ -63,3 +34,33 @@ const postgresRecordMethods = {
 
 
 module.exports = postgresRecordMethods;
+
+// const recordMethods = {
+//   getRecords: (req, res) => {
+//     const username = req.params.username
+//     db.query(aqlQuery`FOR u IN Records FILTER u.email == ${username} RETURN u`)
+//       .then(({ _result }) => res.send(_result))
+//       .catch((err) => {
+//         console.log(err);
+//         res.sendStatus(404)
+//       })
+//   },
+//   createRecords: (req, res) => {
+//     const username = req.params.username;
+//     const newEntry = req.body;
+
+//     db.query(aqlQuery`FOR u IN Records FILTER u.email == ${username} RETURN u`)
+//       .then(({ _result }) => {
+//         const currentEntries = _result[0].entry
+//         console.log(currentEntries);
+//         newEntry['id'] = currentEntries.length + 1;
+//         currentEntries.push(newEntry);
+//         db.query(aqlQuery`FOR u IN Records FILTER u.email == ${username} UPDATE u WITH {entry: ${currentEntries}} IN Records RETURN u`)
+//           .then(() => res.sendStatus(200))
+//       })
+//       .catch((err) => {
+//         console.log(err)
+//         res.sendStatus(404);
+//       })
+//   }
+// };
--- server/routes/timetable.js
+++ server/routes/timetable.js
@@ -1,24 +1,25 @@
-const db = require('../../database');
-const aqlQuery = require('arangojs').aqlQuery;
+// const db = require('../../database');
+// const aqlQuery = require('arangojs').aqlQuery;
+const db = require('../../postgres'); // postgres
 const scheduleKeeper = require('./scheduleKeeper.js')
 
 
-const timeTable = {
+const postgressTimeTable = {
   userTimeTable: {},
   startTimeTable: () => {
     const tempTable = {}
-    db.query('FOR u IN Users RETURN { email: u.email, EOD: u.prompts.EOD }')
-      .then(({ _result }) => {
-        _result.forEach((savedUser) => {
-          if (!savedUser.EOD) {
-            db.query(aqlQuery`FOR u in Users FILTER u.email == ${savedUser.email} REMOVE u IN Users`)
+    db.query('SELECT email, eod FROM users')
+      .then((data) => {
+        data.rows.forEach((savedUser) => {
+          if (!savedUser.eod) {
+            db.query(`DELETE FROM users WHERE email = '${savedUser.email}'`)
               .then(() => {
-                db.query(aqlQuery`FOR u in Records FILTER u.email == ${savedUser.email} REMOVE u IN Records`)
+                db.query(`DELETE FROM records WHERE email = '${savedUser.email}'`)
                   .then(() => console.log('delete incomplete data from Records'))
               })
               .then(() => console.log('delete incomplete data from User'))
           } else {
-            const time = savedUser.EOD.split(':');
+            const time = savedUser.eod.split(':');
             tempTable[savedUser.email] = scheduleKeeper(time, savedUser.email);
           }
           return tempTable;
@@ -31,4 +32,33 @@ const timeTable = {
   }
 }
 
-module.exports = timeTable;
+module.exports = postgressTimeTable;
+
+
+// const timeTable = {
+//   userTimeTable: {},
+//   startTimeTable: () => {
+//     const tempTable = {}
+//     db.query('FOR u IN Users RETURN { email: u.email, EOD: u.prompts.EOD }')
+//       .then(({ _result }) => {
+//         _result.forEach((savedUser) => {
+//           if (!savedUser.EOD) {
+//             db.query(aqlQuery`FOR u in Users FILTER u.email == ${savedUser.email} REMOVE u IN Users`)
+//               .then(() => {
+//                 db.query(aqlQuery`FOR u in Records FILTER u.email == ${savedUser.email} REMOVE u IN Records`)
+//                   .then(() => console.log('delete incomplete data from Records'))
+//               })
+//               .then(() => console.log('delete incomplete data from User'))
+//           } else {
+//             const time = savedUser.EOD.split(':');
+//             tempTable[savedUser.email] = scheduleKeeper(time, savedUser.email);
+//           }
+//           return tempTable;
+//         });
+//       })
+//       .catch((err) => console.log(err))
+//   },
+//   updateTimeTable: (time, username)=>{
+//     userTimeTable[username] = scheduleKeeper(time, username);
+//   }
+// }
