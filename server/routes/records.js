42ecea61ac52063cd6c1d6b0deea953cf6101de5
mbelesiu <matt.belesiu@gmail.com>
2020-12-19 09:46:12 -0800

    fixed last bug with , strings. now all entries should work

--- server/routes/records.js
+++ server/routes/records.js
@@ -7,13 +7,18 @@ const postgresRecordMethods = {
     const email = req.params.username
     db.query(`SELECT records.record_id, users.email, records.date, users.prompts, records.entry FROM records, users WHERE users.email = records.email AND users.email = '${email}'`)
       .then((data) => {
+
         for(let i = 0; i < data.rows.length; i++){
+          // console.log(data.rows[i])
           for ( let j = 0; j < data.rows[i].entry.length; j++){
+            data.rows[i].entry[j] = decodeURIComponent(data.rows[i].entry[j])
             data.rows[i].entry[j] = _.unescape(data.rows[i].entry[j]);
+            data.rows[i].prompts[j] = decodeURIComponent(data.rows[i].prompts[j])
             data.rows[i].prompts[j] = _.unescape(data.rows[i].prompts[j]);
+
+            // console.log(data.rows[i].entry[j])
           }
         }
-
         res.send(data.rows)
       })
       .catch((err) => {
@@ -25,10 +30,14 @@ const postgresRecordMethods = {
     const email = req.params.username;
     const date = req.body.date;
     const newEntry = req.body.entry;
+    // console.log(newEntry)
+    // console.log('----------')
     for(let i = 0; i < newEntry.length; i++){
       newEntry[i] = _.escape(newEntry[i]);
+      newEntry[i] = encodeURIComponent(newEntry[i]);
     }
-    db.query(`INSERT INTO records (email, entry, date) VALUES ('${email}',  '{${newEntry}}' , '${date}')`)
+    // console.log(newEntry)
+    db.query(`INSERT INTO records (email, entry, date) VALUES ('${email}',  $$ {${newEntry}} $$ , '${date}')`)
       .then(() => res.sendStatus(200))
       .catch((err) => {
         console.log(err)
